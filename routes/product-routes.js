e03445702e6e5f8bb287cbe91eb032d128fe6d4d
owujib <48925577+owujib@users.noreply.github.com>
2020-12-09 10:36:03 +0100

    ..

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -9,37 +9,15 @@ const Product = require('../models/Product');
 
 const router = express.Router();
 
-const upload = multer().single('productImg');
+const upload = multer();
 
-router.get('/create', (req, res, next) => {
+router.get('/create', upload.single('productImg'), (req, res, next) => {
   res.render('products/create.ejs', {
     title: 'create product',
   });
 });
 
-router.post('/create', (req, res, next) => {
-  upload(req, res, async (err) => {
-    try {
-      const { file, body } = req;
-      if (err) throw err;
-      //check if file is not an image
-      // if (
-      //   file.detectedFileExtension != '.jpg' ||
-      //   file.detectedFileExtension != '.png'
-      // ) {
-      //   return res.send('you can only upload png and jpg files');
-      // }
-      // await pipeline(
-      //   file.stream,
-      //   fs.createReadStream(`./uploads/${file.originalName}`)
-      // );
-      const product = await Product.create(req.body);
-      res.redirect(`/product/${product._id}`);
-    } catch (error) {
-      next(error);
-    }
-  });
-});
+router.post('/create', (req, res, next) => {});
 
 router.get('/', async (req, res, next) => {
   try {
